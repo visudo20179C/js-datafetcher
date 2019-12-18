752dc29c3ccfe9c3b7f4dad49443f604c333acdd
visudo20179C <50524792+visudo20179C@users.noreply.github.com>
2019-12-18 15:42:32 -0500

    create main js file

---
+++ main.js
@@ -0,0 +1,82 @@
+// Custom Data Fetcher class for WordPress REST API
+// Author - visudo
+// You can use this class to dynamically generate WP page content from page templates
+// This file is open-source and free to distribute, change, and use 
+// However I won't provide much help if you edit this file
+
+export class DataFetcher {
+
+  // Construct ID/type to be used in an instance of this class
+  constructor(id, type) {
+    this.id = id;
+    this.type = type;
+  }
+
+  // grabs JSON data from hard-coded endpoints
+  getJSONData() {
+
+    var myjson = [];
+
+    // add/remove api endpoints here
+    var apis = [];
+
+    this.type.forEach(function(element) {
+      var thisfetch = contains(apis, element);
+  		if (thisfetch != null) {
+  			try {
+  				const res = fetch(thisfetch)
+  				.then(res => res.json())
+  				.then(res => {
+  				  myjson.push(res);
+  				})
+  			}
+  			catch(error) {
+  				console.log(error);
+  			}
+  		}
+    })
+
+    return myjson;
+  }
+
+  // Gets the page HTML from the elem.content.rendered field of the JSON
+  generateHTMLtemplate(data) {
+
+    var mydata = data;
+    var ret = ``;
+
+    if (mydata.length > 1) {
+      mydata.forEach(function(element){
+        element.forEach(function(elem){
+          if (elem.type == "page" || elem.type == "post" || elem.type == "shop_item") {
+            ret += `${elem.content.rendered}`;
+          }
+          else if (elem.type == "valve"){
+            ret += `${elem}`;
+          }
+        })
+      })
+    }
+    else {
+      ret += `${mydata[0].content.rendered}`;
+    }
+
+    return ret;
+  }
+
+  // removes loading div tag and brings content into view
+  bringIntoView(html) {
+	  var load = document.getElementById("loading");
+	  load.remove();
+	  document.getElementById("app").innerHTML = html;
+  }
+
+}
+
+//helper funciton
+function contains(arr, val) {
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].name === val) return arr[i].value;
+  }
+  return null;
+}
